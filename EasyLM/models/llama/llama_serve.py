3aba5efbd84953b1093b7e734443e0ea095002d5
Sharjeel Ahmed <109984369+sharjeel103@users.noreply.github.com>
2024-12-11 12:35:09 +0500

    Update llama_serve.py

--- EasyLM/models/llama/llama_serve.py
+++ EasyLM/models/llama/llama_serve.py
@@ -192,35 +192,7 @@ def main(argv):
         model_ps, get_float_dtype_by_name(FLAGS.param_dtype)
     )
 
-    @partial(
-        pjit,
-        in_shardings=(model_ps, PS(), PS()),
-        out_shardings=(PS(), PS(), PS())
-    )
-    def forward_loglikelihood(params, rng, batch):
-        batch = with_sharding_constraint(batch, PS(('dp', 'fsdp')))
-        rng_generator = JaxRNG(rng)
-        input_tokens = batch['input_tokens']
-        output_tokens = batch['output_tokens']
-        input_mask = batch['input_mask']
-        output_mask = batch['output_mask']
-
-        logits = hf_model.module.apply(
-            params, input_tokens, attention_mask=input_mask,
-            deterministic=True, rngs=rng_generator(LLaMAConfigurator.rng_keys()),
-        ).logits
-        loglikelihood = -optax.softmax_cross_entropy_with_integer_labels(
-            logits, output_tokens
-        )
-        loglikelihood = jnp.sum(loglikelihood * output_mask, axis=-1)
-        match_count = jnp.sum(
-            (jnp.argmax(logits, axis=-1) == output_tokens) * output_mask,
-            axis=-1
-        )
-        total = jnp.sum(output_mask, axis=-1)
-        is_greedy = match_count == total
-        return loglikelihood, is_greedy, rng_generator()
-
+    
 
     @partial(
         pjit,
@@ -251,29 +223,6 @@ def main(argv):
         ).sequences[:, batch['input_tokens'].shape[1]:]
         return output, rng_generator()
 
-    @partial(
-        pjit,
-        in_shardings=(model_ps, PS(), PS()),
-        out_shardings=(PS(), PS())
-    )
-    def forward_greedy_generate(params, rng, batch):
-        batch = with_sharding_constraint(batch, PS(('dp', 'fsdp')))
-        rng_generator = JaxRNG(rng)
-        output = hf_model.generate(
-            batch['input_tokens'],
-            attention_mask=batch['attention_mask'],
-            params=params['params'],
-            prng_key=rng_generator(),
-            generation_config=GenerationConfig(
-                max_new_tokens=FLAGS.seq_length - FLAGS.input_length,
-                pad_token_id=tokenizer.eos_token_id,
-                bos_token_id=tokenizer.bos_token_id,
-                eos_token_id=tokenizer.eos_token_id,
-                do_sample=False,
-                num_beams=1,
-            )
-        ).sequences[:, batch['input_tokens'].shape[1]:]
-        return output, rng_generator()
 
     mesh = LLaMAConfigurator.get_jax_mesh(FLAGS.mesh_dim)
     with mesh:
